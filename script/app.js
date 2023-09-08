ad2a940ca64dfb76341c68bc81b739131fed56c9
Fritz Evar Quije <127073042+Fritz0223@users.noreply.github.com>
2023-09-08 10:43:26 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -14,5 +14,14 @@ function displayDiff() {
   document.getElementById("diffanswer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
+function displaymult() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum * secondNum;
+  document.getElementById("multanswer").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${total}` ;
+}
+
 document.getElementById('sumButton').addEventListener("click", displaySum);
 document.getElementById('diffButton').addEventListener("click", displayDiff);
+document.getElementById('diffButton').addEventListener("click", displaymult);
